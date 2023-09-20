7234a81d8cb315c0e1397bff40b12529bfb2b8a3
javaTpoint <javatpoint@xyz>
2023-09-20 20:20:25 +0800

    登录简单实现cao

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = 'any random string'  # 这里我们直接给定一个密钥
 
 
 @app.route('/hello')
@@ -26,9 +27,26 @@ def newspage():
     return render_template("news.html", data=newsContent)
 
 
-@app.route('/products')  # 增加一个product页面
+@app.route('/product')  # 增加一个product页面
 def productpage():
     return render_template("products.html")
+
+
+@app.route('/login')
+def loginpage():
+    return render_template("login.html")
+
+
+@app.route('/loginProcess', methods=['POST', 'GET'])
+def loginProcesspage():
+    if request.method == 'POST':
+        nm = request.form['nm']
+        pwd = request.form['pwd']
+        if nm == 'cao' and pwd == '123':
+            session['username'] = nm  # 使用session存储方式，session默认为数组，给定key和value即可
+            return redirect(url_for('index'))  # 重定向跳转到首页
+        else:
+            return 'the username or userpwd does not match!'
 
 
 if __name__ == '__main__':
