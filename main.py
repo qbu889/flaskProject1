236fe8637b6bea0857560166ece333567ee5a5b0
javaTpoint <javatpoint@xyz>
2023-09-25 14:12:47 +0800

    (4)

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    msg = "my name is caojianhua, China up!"
+    return render_template("index.html", data=msg)
+
+
+@app.route('/news')  # 增加一个news页面
+def newspage():
+    newsContent = "全国上下一心支持武汉，武汉加油！"
+    return render_template("news.html", data=newsContent)
+
+
+@app.route('/product')  # 增加一个product页面
+def productpage():
+    return render_template("products.html")
+
+
+if __name__ == "__main__":
+    app.run(port=2020, host="127.0.0.1", debug=True)
